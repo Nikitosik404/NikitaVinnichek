74feac16f3dcdd72278d380168d5ff9a2b0efb1f
Nikitosik404 <vinnichek.nikita@gmail.com>
2018-10-26 13:42:24 +0300

    fix hw1

--- src/test/java/hw1/HomeworkTestSoftAssertion.java
+++ src/test/java/hw1/HomeworkTestSoftAssertion.java
@@ -1,6 +1,6 @@
 package hw1;
 
-import org.openqa.selenium.By;
+import base.TestBaseClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -12,14 +12,15 @@ import java.util.concurrent.TimeUnit;
 
 import static enums.ProperTextHomePage.*;
 import static enums.Users.PITER_CHALOVSKII;
-import static java.lang.System.setProperty;
+import static org.openqa.selenium.By.cssSelector;
+import static org.openqa.selenium.By.id;
+import static org.testng.Assert.assertTrue;
 
-public class HomeworkTestSoftAssertion {
+public class HomeworkTestSoftAssertion extends TestBaseClass {
 
     @Test
     public void EpamPageTest() {
-        setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
-        SoftAssert softAssert = new SoftAssert();
+       SoftAssert softAssert = new SoftAssert();
 
         //1 Open BR and navigate
         WebDriver driver = new ChromeDriver();
@@ -31,13 +32,13 @@ public class HomeworkTestSoftAssertion {
         softAssert.assertEquals(driver.getTitle(), "Home Page");
 
         //3 Perform login
-        driver.findElement(By.cssSelector(".profile-photo")).click();
-        driver.findElement(By.cssSelector("[id = 'Name']")).sendKeys(PITER_CHALOVSKII.login);
-        driver.findElement(By.cssSelector("[id = 'Password']")).sendKeys(PITER_CHALOVSKII.password);
-        driver.findElement(By.cssSelector("[type = 'submit']")).click();
+        driver.findElement(cssSelector(".profile-photo")).click();
+        driver.findElement(cssSelector("[id = 'Name']")).sendKeys(PITER_CHALOVSKII.login);
+        driver.findElement(cssSelector("[id = 'Password']")).sendKeys(PITER_CHALOVSKII.password);
+        driver.findElement(cssSelector("[type = 'submit']")).click();
 
         //4 Assert User name in the left-top side of screen that user is loggined
-        WebElement userName = driver.findElement(By.cssSelector(".profile-photo > span"));
+        WebElement userName = driver.findElement(cssSelector(".profile-photo > span"));
         softAssert.assertTrue(userName.isDisplayed());
         softAssert.assertEquals(userName.getText(), "PITER CHAILOVSKII");
 
@@ -45,28 +46,27 @@ public class HomeworkTestSoftAssertion {
         softAssert.assertEquals(driver.getTitle(), "Home Page");
 
         //6 Assert that there are 4 items on the header section are displayed and they have proper texts
+        List<WebElement> headerItems = driver.findElements(cssSelector("ul.uui-navigation.nav.navbar-nav.m-l8 > li"));
+        softAssert.assertEquals(headerItems.size(), 4);
 
-        //6.1 Assertion that 4 items are displayed
-        softAssert.assertTrue(driver.findElement(By.cssSelector(".uui-navigation a[href*='index']")).isDisplayed());
-        softAssert.assertTrue(driver.findElement(By.cssSelector(".uui-navigation a[href*='contacts']")).isDisplayed());
-        softAssert.assertTrue(driver.findElement(By.cssSelector(".uui-navigation .dropdown-toggle")).isDisplayed());
-        softAssert.assertTrue(driver.findElement(By.cssSelector(".uui-navigation a[href*='metals-colors']")).isDisplayed());
+        for (WebElement item: headerItems){
+            softAssert.assertTrue(item.isDisplayed());
+        }
 
-        //6.2 Assertion that 4 items have proper texts
-        softAssert.assertEquals(driver.findElement(By.cssSelector(".uui-navigation a[href*='index']")).getText(), "HOME");
-        softAssert.assertEquals(driver.findElement(By.cssSelector(".uui-navigation a[href*='contacts']")).getText(), "CONTACT FORM");
-        softAssert.assertEquals(driver.findElement(By.cssSelector(".uui-navigation .dropdown-toggle")).getText(), "SERVICE");
-        softAssert.assertEquals(driver.findElement(By.cssSelector(".uui-navigation a[href*='metals-colors']")).getText(), "METALS & COLORS");
+        softAssert.assertEquals(headerItems.get(0).getText(), "HOME");
+        softAssert.assertEquals(headerItems.get(1).getText(), "CONTACT FORM");
+        softAssert.assertEquals(headerItems.get(2).getText(), "SERVICE");
+        softAssert.assertEquals(headerItems.get(3).getText(), "METALS & COLORS");
 
         //7 Assert that there are 4 images on the Index Page and they are displayed
-        List<WebElement> imagesItems = driver.findElements(By.cssSelector(".benefit-icon"));
+        List<WebElement> imagesItems = driver.findElements(cssSelector(".benefit-icon"));
         softAssert.assertEquals(imagesItems.size(), 4);
         for (WebElement item : imagesItems) {
-            softAssert. assertTrue(item.isDisplayed());
+            assertTrue(item.isDisplayed());
         }
 
         //8 Assert that there are 4 texts on the Index Page under icons and they have proper text
-        List<WebElement> textItems = driver.findElements(By.cssSelector(".benefit-txt"));
+        List<WebElement> textItems = driver.findElements(cssSelector(".benefit-txt"));
         softAssert.assertEquals(textItems.size(), 4);
         softAssert.assertEquals(textItems.get(0).getText(), PRACTICE.text);
         softAssert.assertEquals(textItems.get(1).getText(), CUSTOM.text);
@@ -74,33 +74,32 @@ public class HomeworkTestSoftAssertion {
         softAssert.assertEquals(textItems.get(3).getText(), BASE.text);
 
         //9 Assert a text of the main header
-        softAssert.assertEquals(driver.findElement(By.cssSelector(".main-title")).getText(), "EPAM FRAMEWORK WISHES…");
+        softAssert.assertEquals(driver.findElement(cssSelector(".main-title")).getText(), "EPAM FRAMEWORK WISHES…");
         String mainTxt = "LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISICING ELIT, SED DO EIUSMOD TEMPOR INCIDIDUNT UT LABORE ET DOLORE MAGNA ALIQUA. UT ENIM AD MINIM VENIAM, QUIS NOSTRUD EXERCITATION ULLAMCO LABORIS NISI UT ALIQUIP EX EA COMMODO CONSEQUAT DUIS AUTE IRURE DOLOR IN REPREHENDERIT IN VOLUPTATE VELIT ESSE CILLUM DOLORE EU FUGIAT NULLA PARIATUR.";
-        softAssert.assertEquals(driver.findElement(By.cssSelector(".main-txt")).getText(), mainTxt);
+        softAssert.assertEquals(driver.findElement(cssSelector(".main-txt")).getText(), mainTxt);
 
 
         //10 Assert that there is the iframe in the center of page
-        softAssert.assertTrue(driver.findElement(By.cssSelector("iframe")).isDisplayed());
+        softAssert.assertTrue(driver.findElement(cssSelector("iframe")).isDisplayed());
 
         //11 Switch to the iframe and check that there is Epam logo in the left top conner of iframe
-        String mainHandler = driver.getWindowHandle();
-        driver.switchTo().frame(driver.findElement(By.cssSelector("iframe")));
-        softAssert.assertTrue(driver.findElement(By.cssSelector(".epam-logo")).isDisplayed());
+        driver.switchTo().frame("iframe");
+        softAssert.assertTrue(driver.findElement( id("epam_logo") ).isDisplayed());
 
         //12 Switch to original window back
-        driver.switchTo().window(mainHandler);
+        driver.switchTo().defaultContent();
 
         //13 Assert a text of the sub header
-        softAssert.assertEquals(driver.findElement(By.cssSelector("[class='text-center']")).getText(), "JDI GITHUB");
+        softAssert.assertEquals(driver.findElement(cssSelector("[class='text-center']")).getText(), "JDI GITHUB");
 
         //14 Assert that JDI GITHUB is a link and has a proper URL
-        softAssert.assertEquals(driver.findElement(By.cssSelector("[class='text-center']>a")).getAttribute("href"), "https://github.com/epam/JDI");
+        softAssert.assertEquals(driver.findElement(cssSelector("[class='text-center']>a")).getAttribute("href"), "https://github.com/epam/JDI");
 
         //15 Assert that there is Left Section
-        softAssert.assertTrue(driver.findElement(By.cssSelector("[name='navigation-sidebar']")).isDisplayed());
+        softAssert.assertTrue(driver.findElement(cssSelector("[name='navigation-sidebar']")).isDisplayed());
 
         //16 Assert that there is Footer
-        softAssert.assertTrue(driver.findElement(By.cssSelector("footer")).isDisplayed());
+        softAssert.assertTrue(driver.findElement(cssSelector("footer")).isDisplayed());
 
         //17 Close Browser
         driver.close();
